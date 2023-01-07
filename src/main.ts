c9ee3967c273cf8c76d12880e506bf58da908eff
JSGitHubbing <jesus.ortega.miguel@gmail.com>
2023-01-07 13:14:49 +0100

    Configured swagger to generate API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const packageInfo = require('../package.json');
+
   const fs = require('fs');
   const keyFile = fs.readFileSync(__dirname + '/../ssl/localhost.key');
   const certFile = fs.readFileSync(__dirname + '/../ssl/localhost.crt');
@@ -12,6 +16,17 @@ async function bootstrap() {
       cert: certFile,
     },
   });
+
+  // SWAGGER CONFIG
+  const config = new DocumentBuilder()
+    .setTitle(packageInfo.name)
+    .setDescription(packageInfo.description)
+    .setVersion(packageInfo.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
